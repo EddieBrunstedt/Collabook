61986893822cad7bf9d934e1d0859fcc9c34aad7
Eddie Brunstedt <eddie.brunstedt@gmail.com>
2018-01-17 01:36:28 +0100

    Fixed incorrect copy/paste from another file

--- routes/index.js
+++ routes/index.js
@@ -4,68 +4,7 @@ const router = express.Router();
 //Import controllers
 const rootControllers = require('../controllers/rootControllers');
 
-/* GET home page. */const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-
-// User Schema
-const UserSchema = mongoose.Schema({
-  email: {
-    type: String,
-    index: true,
-    required: 'E-mail is required.',
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  name: {
-    type: String,
-    required: 'A name is required.'
-  },
-  password: {
-    type: String,
-    required: 'A password is required.'
-  },
-  bio: {
-    type: String,
-    default: ''
-  }
-}, {runSettersOnQuery: true});
-
-const User = module.exports = mongoose.model('User', UserSchema);
-
-module.exports.createUser = (newUser) => {
-  bcrypt.genSalt(10)
-    .then((salt) => {
-      return bcrypt.hash(newUser.password, salt)
-    })
-    .then((hash) => {
-      newUser.password = hash;
-      newUser.save();
-    })
-    .catch((err) => {
-      throw err;
-    })
-};
-
-module.exports.getUserById = (id) => {
-  return User.findById(id);
-};
-
-module.exports.getUserByEmail = (email) => {
-  return User.findOne({email: email});
-};
-
-module.exports.comparePassword = (candidatePassword, hash) => {
-  return bcrypt.compare(candidatePassword, hash);
-};
-
-module.exports.updateUserAccount = (id, updates) => {
-  return User.findOneAndUpdate(
-    {_id: id},
-    {$set: updates},
-    {new: true, runValidators: true, context: 'query'}
-  )
-};
+/* GET home page. */
 router.get('/', rootControllers.homePage);
 
 
